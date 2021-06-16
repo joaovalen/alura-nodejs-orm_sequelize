efdaecd723348372b0b0ca488bb7c17c791e612e
joaovalen <joaovpvalentini@gmail.com>
2021-06-16 11:46:15 -0300

    parte 1 curso

---
+++ api/index.js
@@ -0,0 +1,18 @@
+// npm install express mysql2 sequelize sequelize-cli 
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+const port = 3000
+
+app.get('/teste', (req, res) => {
+    res.status(200)
+    res.send({mensagem: "A API está funcionando"})
+})
+
+app.listen(port, () => console.log(`Servidor está rodando na porta ${port}`))
+
+module.exports = app
